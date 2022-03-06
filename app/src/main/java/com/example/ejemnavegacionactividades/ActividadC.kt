cce711844afe89efa4ada5bab863c47cff6d3eb5
HeynerPat <72895524+HeynerPat@users.noreply.github.com>
2022-03-06 18:52:46 -0500

    Errores de navegación

--- app/src/main/java/com/example/ejemnavegacionactividades/ActividadC.kt
+++ app/src/main/java/com/example/ejemnavegacionactividades/ActividadC.kt
@@ -1,11 +1,24 @@
 package com.example.ejemnavegacionactividades
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class ActividadC : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_actividad_c)
+
+        val mensaje = intent.getStringExtra("MENSAJE")
+        val mensajeDeA = intent.getStringExtra("MENSAJEA")
+        val botonn = findViewById<Button>(R.id.botonn)
+
+        Toast.makeText(this,mensaje, Toast.LENGTH_SHORT).show()
+
+        botonn.setOnClickListener{
+            finish()
+        }
     }
 }
